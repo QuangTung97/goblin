9371ebc92776f552383ee387eef81c7da8a3f0b9
QuangTung97 <quangtung29121997@gmail.com>
2021-06-18 11:14:24 +0700

    Watch nodes

--- client.go
+++ client.go
@@ -1,9 +1,17 @@
 package goblin
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"github.com/QuangTung97/goblin/goblinpb"
 	"google.golang.org/grpc"
+	"io"
+	"log"
+	"strconv"
+	"strings"
 	"sync/atomic"
+	"time"
 	"unsafe"
 )
 
@@ -13,7 +21,7 @@ var ErrNoConn = errors.New("no connection available")
 // ClientConfig for config client pooling
 type ClientConfig struct {
 	Addresses []string
-	Options   []grpc.CallOption
+	Options   []grpc.DialOption
 }
 
 type clientConn struct {
@@ -36,13 +44,68 @@ type PoolClient struct {
 
 // NewPoolClient ...
 func NewPoolClient(config ClientConfig) *PoolClient {
-	return makePoolClient(config)
+	client := makePoolClient(config)
+	go client.watchNodes()
+	return client
 }
 
 func makePoolClient(config ClientConfig) *PoolClient {
 	return &PoolClient{
 		config: config,
 	}
+}
+
+func (c *PoolClient) watchNodesSingleLoop(addr string) {
+	conn, err := grpc.Dial(addr, c.config.Options...)
+	if err != nil {
+		log.Println("[ERROR] dial for watch nodes error:", err)
+		time.Sleep(60 * time.Second)
+		return
+	}
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	client := goblinpb.NewGoblinServiceClient(conn)
+	stream, err := client.Watch(context.Background(), &goblinpb.WatchRequest{})
+	if err != nil {
+		log.Println("[ERROR] watch nodes error:", err)
+		time.Sleep(60 * time.Second)
+		return
+	}
+
+	for {
+		nodeList, err := stream.Recv()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Println("[ERROR] receive nodes error:", err)
+			time.Sleep(60 * time.Second)
+			return
+		}
+
+		c.handleNewNodeList(nodeList.Nodes)
+	}
+}
+
+func (c *PoolClient) watchNodes() {
+	index := 0
+	for ; ; index = (index + 1) % len(c.config.Addresses) {
+		addr := c.config.Addresses[index]
+		c.watchNodesSingleLoop(addr)
+	}
+}
+
+func (c *PoolClient) handleNewNodeList(nodes []*goblinpb.Node) {
+	newClientConns := computeNewClientConns(c.getClientConns(), nodes, 2000, func(addr string) *grpc.ClientConn {
+		conn, err := grpc.Dial(addr, c.config.Options...)
+		if err != nil {
+			panic(err)
+		}
+		return conn
+	})
+	c.setClientConns(newClientConns)
 }
 
 func doRequestConn(conn *clientConn, fn func(conn *grpc.ClientConn) error) error {
@@ -55,7 +118,7 @@ func doRequestConn(conn *clientConn, fn func(conn *grpc.ClientConn) error) error
 	return fn(conn.conn)
 }
 
-// GetConn get a connection from pool, DO *NOT* use conn outside the lifetime of current goroutine
+// GetConn get a connection from pool, DO *NOT* use conn outside the lifetime of current function
 func (c *PoolClient) GetConn(fn func(conn *grpc.ClientConn) error) error {
 	for {
 		conn, ok := c.getNextConn()
@@ -77,17 +140,6 @@ func (c *PoolClient) getClientConns() *clientConns {
 
 func (c *PoolClient) setClientConns(conns *clientConns) {
 	atomic.StorePointer(&c.conns, unsafe.Pointer(conns))
-}
-
-func cloneClientConns(conns *clientConns) *clientConns {
-	result := &clientConns{}
-
-	result.conns = make([]*clientConn, 0, len(conns.conns))
-	for _, c := range conns.conns {
-		result.conns = append(result.conns, c)
-	}
-
-	return result
 }
 
 func (c *clientConn) acquire() (ok bool) {
@@ -123,4 +175,61 @@ func (c *PoolClient) getNextConn() (*clientConn, bool) {
 
 	index := (newVal - 1) % uint64(len(conns))
 	return conns[index], true
+}
+
+func getGRPCAddrFromMemberlist(addr string, portDiff int) string {
+	list := strings.Split(addr, ":")
+	if len(list) < 2 {
+		panic("invalid address")
+	}
+	port, err := strconv.Atoi(list[1])
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%s:%d", list[0], port-portDiff)
+}
+
+func computeNewClientConns(
+	old *clientConns, nodes []*goblinpb.Node, portDiff int,
+	dial func(addr string) *grpc.ClientConn,
+) *clientConns {
+	oldNameSet := map[string]struct{}{}
+	for _, conn := range old.conns {
+		oldNameSet[conn.nodeName] = struct{}{}
+	}
+
+	newNameSet := map[string]struct{}{}
+	for _, node := range nodes {
+		newNameSet[node.Name] = struct{}{}
+	}
+
+	result := &clientConns{}
+	result.conns = make([]*clientConn, 0, len(old.conns))
+	for _, conn := range old.conns {
+		_, existed := newNameSet[conn.nodeName]
+		if !existed {
+			needClose := conn.release()
+			if needClose {
+				_ = conn.conn.Close()
+			}
+			continue
+		}
+
+		result.conns = append(result.conns, conn)
+	}
+
+	for _, node := range nodes {
+		_, existed := oldNameSet[node.Name]
+		if existed {
+			continue
+		}
+
+		result.conns = append(result.conns, &clientConn{
+			conn:     dial(getGRPCAddrFromMemberlist(node.Addr, portDiff)),
+			nodeName: node.Name,
+			refCount: 1,
+		})
+	}
+
+	return result
 }
--- client_test.go
+++ client_test.go
@@ -1,7 +1,9 @@
 package goblin
 
 import (
+	"github.com/QuangTung97/goblin/goblinpb"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 	"testing"
 	"unsafe"
 )
@@ -9,26 +11,6 @@ import (
 func TestValidateSizeOfClientConn(t *testing.T) {
 	assert.Equal(t, uintptr(64), unsafe.Sizeof(clientConn{}))
 	assert.Equal(t, uintptr(8), unsafe.Alignof(clientConn{}))
-}
-
-func TestCloneClientConns(t *testing.T) {
-	conns := &clientConns{
-		conns: []*clientConn{
-			{
-				nodeName: "node-1",
-				refCount: 10,
-			},
-			{
-				nodeName: "node-2",
-				refCount: 20,
-			},
-		},
-	}
-	result := cloneClientConns(conns)
-	if result == conns {
-		t.Error("should not be equal")
-	}
-	assert.Equal(t, conns, result)
 }
 
 func TestClientConnAcquireRelease(t *testing.T) {
@@ -113,4 +95,83 @@ func TestPoolClient_GetNextConn_Conns_Empty(t *testing.T) {
 	result, ok := pool.getNextConn()
 	assert.Equal(t, false, ok)
 	assert.Equal(t, (*clientConn)(nil), result)
+}
+
+func TestGetGRPCAddrFromMemberlistAddr(t *testing.T) {
+	t.Run("not-contains-colon", func(t *testing.T) {
+		assert.PanicsWithValue(t, "invalid address", func() {
+			getGRPCAddrFromMemberlist("some-host-1", 200)
+		})
+	})
+
+	t.Run("not-number", func(t *testing.T) {
+		assert.Panics(t, func() {
+			getGRPCAddrFromMemberlist("some-host-1:string", 200)
+		})
+	})
+
+	t.Run("normal", func(t *testing.T) {
+		result := getGRPCAddrFromMemberlist("some-host-1:5800", 200)
+		assert.Equal(t, "some-host-1:5600", result)
+	})
+}
+
+func TestComputeNewClientConns(t *testing.T) {
+	conn1 := &clientConn{
+		nodeName: "name-1",
+		refCount: 10,
+	}
+	conn2 := &clientConn{
+		nodeName: "name-2",
+		refCount: 20,
+	}
+
+	old := &clientConns{
+		conns: []*clientConn{
+			conn1,
+			conn2,
+		},
+	}
+	nodes := []*goblinpb.Node{
+		{
+			Name: "name-1",
+			Addr: "some-host-1:5800",
+		},
+		{
+			Name: "name-3",
+			Addr: "some-host-3:5800",
+		},
+		{
+			Name: "name-4",
+			Addr: "some-host-4:5800",
+		},
+	}
+
+	var dialAddr string
+	var dialCount int
+	result := computeNewClientConns(old, nodes, 200, func(addr string) *grpc.ClientConn {
+		dialCount++
+		dialAddr = addr
+		return nil
+	})
+
+	assert.NotNil(t, result)
+	if result == old {
+		t.Error("must not be same")
+	}
+	assert.Equal(t, 2, dialCount)
+	assert.Equal(t, "some-host-4:5600", dialAddr)
+	assert.Equal(t, []*clientConn{
+		conn1,
+		{
+			nodeName: "name-3",
+			refCount: 1,
+		},
+		{
+			nodeName: "name-4",
+			refCount: 1,
+		},
+	}, result.conns)
+
+	assert.Equal(t, uint64(19), conn2.refCount)
 }
